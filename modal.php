a0224cea0e11cb1469bc794997388893af60982b
polisek <107623238+polisek@users.noreply.github.com>
2022-12-05 11:38:09 +0100

    Create modal.php

---
+++ modal.php
@@ -0,0 +1,9 @@
+<?php
+    if ( is_user_logged_in() ) {
+        ?>
+        <div id="modal_popup_company" class="modal modal-box_popup">
+            <img src="<?php echo get_template_directory_uri()?>/img/celoagrenturnivolno_unifer_banner.jpg">
+        </div>
+        <?php
+    }
+?> 
